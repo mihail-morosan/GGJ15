28c51eec6e72f10054c50e84c480d3a449e1306b
abiaco <iacob.andrei986@gmail.com>
2015-01-24 10:39:17 +0000

    Human pests walkign around

---
+++ Assets/GenerateHumans.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateHumans : MonoBehaviour {
+    public int number;
+    int current_no;
+    public GameObject human;
+	// Use this for initialization
+	void Start () {
+        Generate(number);
+        current_no = number;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Generate(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Vector3 posgen = transform.localPosition;
+            print(posgen.x + " " + posgen.z);
+            posgen.x += 2;
+            posgen.y += 4;
+            GameObject go = Instantiate(human, posgen, Quaternion.identity) as GameObject;
+            int factor = Random.Range(1, 3) == 1 ? -1 : 1;
+            go.SendMessage("InitWalkSpeed", factor);
+        }
+    }
+
+    
+}
---
+++ Assets/HumanBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanBehaviour : MonoBehaviour {
+    public float WalkSpeed;
+	// Use this for initialization
+	void Start () {
+        //WalkSpeed = 100f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rigidbody.velocity = transform.forward * WalkSpeed;
+	}
+
+    void turnLeft()
+    {
+        transform.Rotate(0, -90, 0);
+    }
+
+    void turnRight()
+    {
+        this.transform.Rotate(0, 90, 0);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "left")
+        {
+            turnLeft();
+        }
+        if (col.gameObject.tag == "right")
+        {
+            int num = Random.Range(1, 5);
+            if( num == 3)
+              turnRight();
+        }
+        if (col.gameObject.tag == "lr")
+        {
+            if (WalkSpeed < 0) turnRight();
+            else turnLeft();
+        }
+        if (col.gameObject.tag == "ai_car")
+        {
+            
+            
+            print("play death sound");
+            Destroy(this);
+        }
+        if (col.gameObject.tag == "car")
+        {
+            print("scream stuff");
+            print("stain car with blood!");
+            Destroy(this);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Physics.IgnoreCollision(col.collider, collider);
+        }
+    }
+
+    void InitWalkSpeed(int factor)
+    {
+        WalkSpeed *= factor;
+    }
+}
