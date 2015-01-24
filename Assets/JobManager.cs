970ab16adfdad224ec827a1ac8f1bf8165e49a77
abiaco <iacob.andrei986@gmail.com>
2015-01-24 16:07:30 +0000

    updated JobManager script

--- Assets/JobManager.cs
+++ Assets/JobManager.cs
@@ -25,7 +25,9 @@ public class JobManager : MonoBehaviour {
         if (checkProxi(player,target))
             if (checkStop(player))
             {
-               
+               // jobStatus: 0 = looking for customer
+                //           1 = going to destination
+                //           2 = delivered
                 if (jobStatus == 1)
                 {   jobStatus = 2;
                     return;
@@ -61,5 +63,14 @@ public class JobManager : MonoBehaviour {
         waypoints[1] = new Vector3(-1f, 3f, -4.3f);
         waypoints[2] = new Vector3(0.6f, 3f, 4.2f);
         waypoints[3] = new Vector3(-1.2f, 3f, 5f);
+    }
+    void OnCollisionEntry(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            // score -
+            newTarget = Instantiate(target, waypoints[Random.Range(0, waypoints.Length)], Quaternion.identity) as GameObject;
+            jobStatus = 0;
+        }
     }
 }
