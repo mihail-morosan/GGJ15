cb54772919a730280f8b4d7eb0e85fa26c67a45e
Mihail Morosan <me@morosanmihail.com>
2015-01-24 17:51:28 +0000

    Added time bonuses (or penalties) to hitting stuff. Fixed some timer gui stuff.

--- Assets/TimerGUI.cs
+++ Assets/TimerGUI.cs
@@ -5,50 +5,7 @@ using System.Collections;
 
 public class TimerGUI : MonoBehaviour {
 
-    private float startTime;
 
-    private int restSeconds;
-
-    private int roundedRestSeconds;
-
-    private float displaySeconds;
-
-    private float displayMinutes;
-
-    public int CountDownSeconds;
-
-    public String LevelToLoadOnComplete;
-
-    void Awake() { startTime = Time.time; }
-
-    void OnGUI()
-    { 
-        float guiTime = Time.time - startTime;
-
-        restSeconds = CountDownSeconds - (int)(guiTime);
-
-        //display messages or whatever here --&gt;do stuff based on your timer
-        if (restSeconds == 60)
-        {
-            print("One Minute Left");
-        }
-        if (restSeconds == 0)
-        {
-            print("Time is Over");
-            //do stuff here
-
-            if(LevelToLoadOnComplete.Length > 0)
-                Application.LoadLevel(LevelToLoadOnComplete);
-        }
-
-        //display the timer
-        roundedRestSeconds = Mathf.CeilToInt(restSeconds);
-        displaySeconds = roundedRestSeconds % 60;
-        displayMinutes = roundedRestSeconds / 60;
-
-        String text = String.Format("{0:00}:{1:00}", displayMinutes, displaySeconds);
-        GUI.Label(new Rect(400, 25, 100, 30), text);
-    }
 
 	// Use this for initialization
 	void Start () {
