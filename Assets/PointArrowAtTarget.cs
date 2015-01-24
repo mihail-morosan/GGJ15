b8fda33853afe7a3cf0414624e13cdd81ad6d3b2
Mihail Morosan <me@morosanmihail.com>
2015-01-24 16:40:45 +0000

    Arrows point in the direction of the chosen... thing.

---
+++ Assets/PointArrowAtTarget.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointArrowAtTarget : MonoBehaviour
+{
+
+    public GameObject TargetObject;
+    public GameObject Arrow;
+
+    public float RotateSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+    
+    void Update()
+    {
+        if (Arrow == null || TargetObject == null)
+            return;
+
+        //Arrow.transform.LookAt(TargetObject.transform);
+
+        Vector3 targetDir = TargetObject.transform.position - transform.position;
+        targetDir.y = 0;
+        float step = RotateSpeed * Time.deltaTime;
+        Vector3 newDir = Vector3.RotateTowards(Arrow.transform.forward, targetDir, step, 0.0F);
+        //Debug.DrawRay(transform.position, TargetObject.transform.position, Color.red);
+
+        //newDir.x = 0;
+        //newDir.z = 0;
+        
+        Arrow.transform.rotation = Quaternion.LookRotation(newDir);
+
+
+        Arrow.transform.position = transform.position;
+    }
+}
